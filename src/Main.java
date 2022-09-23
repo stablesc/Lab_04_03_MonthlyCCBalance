cee09555d270f04614c5e1c9ae3a3d9e131938e3
Steele <stablesc@mail.uc.edu>
2022-09-23 00:23:57 -0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        double initialBalance = 5000;
+        double interestRate = .17;
+        double monthOneInterest = initialBalance * interestRate;
+        double monthTwoBalance = initialBalance + monthOneInterest;
+        double monthTwoInterest = monthTwoBalance * interestRate;
+
+        {
+            System.out.println("With an initial balance of $" + initialBalance + ", and an interest rate of " + interestRate
+                    + ", the interest due after 1 month with no payments made would be $" + monthOneInterest + ".");
+        }
+        {
+                System.out.println("The interest due after two months with no payments would be $" + monthTwoInterest + ".");
+
+        }
+
     }
 }
